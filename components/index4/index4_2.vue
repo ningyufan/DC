6c656ced79f880448f5304760b8106eff79b2ac1
lushimei <lushimei@email.com>
2019-06-10 22:21:58 +0800

    fdjbvgbjmsds

--- components/index4/index4_2.vue
+++ components/index4/index4_2.vue
@@ -1,15 +1,312 @@
 <template>
-    <div class="index4_2">
-        index4_2
+    <div class="digitalGx" style="margin-left: 10vh;margin-right: 10vh">
+        <br>
+        <div class="icon-warp">
+       <i class="el-icon-edit" style="font-size: 20px"> <b>构建智能制造产业生态</b></i>
+        </div>
+       <hr>
+        <br>
+        <el-row :gutter="20">
+            <el-col :span="14">
+            <div class="grid-content1 bg-purple1">
+                <el-row >
+                    <el-col :span="8">
+                    <div class="grid-content1 bg-purple1" style="margin-right:2vh">
+                        <el-image style="width: 100%; height:130px" :src="src1"></el-image>
+                    </div>
+                    </el-col>
+                    <el-col :span="16">
+                    <div class="grid-content1 bg-purple-light1">
+                        <b style="font-size:18px">系统布局，统筹推进</b>
+                        <p style="font-size:15px">把握数字经济发展趋势，加强体系化部署，有序推进产业、创新、市场和治理体系建设，构建广西数字经济生态。深化部门协调和区市联动，引导各市合理布局，加强产、学、研、用协调发展，形成差异布局、协同共进、分工合作的良性局面，凝聚合力推动广西数字经济加快发展。</p>
+                        <el-button type="primary" size="small"><a href="http://www.gxzf.gov.cn/zwgk/zfwj/20180917-713374.shtml" target="_blank">查看详情</a></el-button>
+                    </div>
+                    </el-col>
+                </el-row>
+                <br>
+                <hr>
+                <br>
+                <el-row >
+                    <el-col :span="8">
+                    <div class="grid-content1 bg-purple1" style="margin-right:2vh">
+                        <el-image style="width: 100%; height:130px" :src="src2"></el-image>
+                    </div>
+                    </el-col>
+                    <el-col :span="16">
+                    <div class="grid-content1 bg-purple-light1">
+                        <b style="font-size:18px">突出特色，强调融合</b>
+                        <p style="font-size:15px">发挥我区的资源禀赋和比较优势，鼓励各市根据各自特色特点聚焦重点领域，集中要素资源，形成广西特色鲜明的数字经济比较优势。把握产业融合作为数字经济发展的主引擎地位，坚持互联网、大数据、人工智能等数字技术与实体经济融合发展，支撑构建现代化经济体系。</p>
+                        <el-button type="primary" size="small"><a href="http://www.gxzf.gov.cn/zwgk/zfwj/20180917-713374.shtml" target="_blank">查看详情</a></el-button>
+                    </div>
+                    </el-col>
+                </el-row>
+                <br>
+                <hr>
+                <br>
+                <el-row >
+                    <el-col :span="8">
+                    <div class="grid-content1 bg-purple1" style="margin-right:2vh">
+                        <el-image style="width: 100%; height:130px" :src="src3"></el-image>
+                    </div>
+                    </el-col>
+                    <el-col :span="16">
+                    <div class="grid-content1 bg-purple-light1">
+                        <b style="font-size:18px">政府引导，市场主导</b>
+                        <p style="font-size:15px">充分发挥市场在资源配置中的决定性作用，更好发挥政府顶层设计和规范引导作用，加快转变政府职能，深入推进简政放权、放管结合、优化服务改革，激发各类市场主体的积极性，以市场需求为导向加快各领域数字化产品培育和应用示范，为数字经济发展营造良好市场环境。</p>
+                        <el-button type="primary" size="small"><a href="http://www.gxzf.gov.cn/zwgk/zfwj/20180917-713374.shtml" target="_blank">查看详情</a></el-button>
+                    </div>
+                    </el-col>
+                </el-row>
+                <br>
+                <hr>
+                <br>
+                <el-row >
+                    <el-col :span="8">
+                    <div class="grid-content1 bg-purple1" style="margin-right:2vh">
+                        <el-image style="width: 100%; height:130px" :src="src1"></el-image>
+                    </div>
+                    </el-col>
+                    <el-col :span="16">
+                    <div class="grid-content1 bg-purple-light1">
+                        <b style="font-size:18px">深化改革，创新驱动</b>
+                        <p style="font-size:15px">把改革创新贯穿发展全过程，推进行政管理体制和经济体制改革，破除不利于数字经济科学发展的体制机制障碍，加快数字经济技术、产品、管理、服务、模式等多种形式创新，坚持发挥企业作为创新主体的积极作</p>
+                        <el-button type="primary" size="small"><a href="http://www.gxzf.gov.cn/zwgk/zfwj/20180917-713374.shtml" target="_blank">查看详情</a></el-button>
+                    </div>
+                    </el-col>
+                </el-row>
+                <br>
+                <hr>
+                <br>
+                <el-row >
+                    <el-col :span="8">
+                    <div class="grid-content1 bg-purple1" style="margin-right:2vh">
+                        <el-image style="width: 100%; height:130px" :src="src2"></el-image>
+                    </div>
+                    </el-col>
+                    <el-col :span="16">
+                    <div class="grid-content1 bg-purple-light1">
+                        <b style="font-size:18px">开放带动，合作共赢</b>
+                        <p style="font-size:15px">立足“三大定位”新使命要求，以中国—东盟信息港建设为抓手，全面深化开放合作，加强与东盟沿线国家数字经济合作和面向东盟的产能输出，推进与粤港澳、长三角和西南中南等地区的产业合作，促进对外开放和对内合作的互动，加快培育发展开放型数字经济。</p>
+                        <el-button type="primary" size="small"><a href="http://www.gxzf.gov.cn/zwgk/zfwj/20180917-713374.shtml" target="_blank">查看详情</a></el-button>
+                    </div>
+                    </el-col>
+                </el-row>
+                <br>
+                <hr>
+                <br>
+                <el-row >
+                    <el-col :span="8">
+                    <div class="grid-content1 bg-purple1" style="margin-right:2vh">
+                        <el-image style="width: 100%; height:130px" :src="src3"></el-image>
+                    </div>
+                    </el-col>
+                    <el-col :span="16">
+                    <div class="grid-content1 bg-purple-light1">
+                        <b style="font-size:18px">绿色低碳，安全可控</b>
+                        <p style="font-size:15px">坚持新一代信息技术产业绿色、循环和低碳发展，强化以数字化改造提升传统产业，促进资源共享集约，实现数字经济发展规模、质量、效益的有机统一。统筹协调好发展和安全之间的关系，以安全保发展、以发展促安全，推进行业自律和社会监督，加快健全关键信息基础设施安全保障体系，在发展中提升信息安全保障能力。</p>
+                        <el-button type="primary" size="small"><a href="http://www.gxzf.gov.cn/zwgk/zfwj/20180917-713374.shtml" target="_blank">查看详情</a></el-button>
+                    </div>
+                    </el-col>
+                </el-row>
+            </div>
+            </el-col>
+            <div class="icon-warp">
+                <i class="el-icon-view" style="font-size: 20px" > <b>成果展示</b></i>
+            </div>
+            <el-col :span="10">
+            <div class="grid-content1 bg-purple1">
+                <el-row >
+                    
+                    <el-carousel :interval="4000" type="card" height="200px">
+                        <el-carousel-item><el-image style="width: 100%; height:100%" :src="src4"></el-image></el-carousel-item>
+                        <el-carousel-item><el-image style="width: 100%; height:100%" :src="src5"></el-image></el-carousel-item>
+                        <el-carousel-item><el-image style="width: 100%; height:100%" :src="src6"></el-image></el-carousel-item>
+                    </el-carousel>
+                </el-row>              
+            </div>
+            </el-col>
+            <br>
+            <div class="icon-warp">
+                <i class="el-icon-view" style="font-size: 20px" > <b>数字广西动态</b></i>
+            </div>
+            <el-col :span="10">
+            <div class="grid-content1 bg-purple1">
+                <el-row >
+                    <!-- <el-col :span="8">
+                    <div class="grid-content1 bg-purple1" style="margin-right:2vh">
+                        <el-image style="width: 100%; height:130px" :src="src2"></el-image>
+                    </div>
+                    </el-col> -->
+                    <el-col :span="24">
+                    <div class="grid-content1 bg-purple-light1">
+                        <!-- <b style="font-size:18px">开放带动，合作共赢</b> -->
+                        <p style="font-size:15px">广西数字政务一体化平台与国家政务服务平台对接进度全国第一 “一网通办”让政务服务大提速再提质</p>
+                        <el-button type="primary" size="small"><a href="http://www.gxzf.gov.cn/gxyw/20190604-750934.shtml" target="_blank">查看详情</a></el-button>
+                    </div>
+                    </el-col>
+                </el-row>
+                <br>
+                <hr>
+                <br>
+                <el-row >
+                    <!-- <el-col :span="8">
+                    <div class="grid-content1 bg-purple1" style="margin-right:2vh">
+                        <el-image style="width: 100%; height:130px" :src="src2"></el-image>
+                    </div>
+                    </el-col> -->
+                    <el-col :span="24">
+                    <div class="grid-content1 bg-purple-light1">
+                        <!-- <b style="font-size:18px">开放带动，合作共赢</b> -->
+                        <p style="font-size:15px">广西数字政务一体化平台与国家政务服务平台深入对接荣登全国第一</p>
+                        <el-button type="primary" size="small"><a href="http://www.gxzf.gov.cn/zt/jd/szgxzl/gdxw/20190601-750635.shtml" target="_blank">查看详情</a></el-button>
+                    </div>
+                    </el-col>
+                </el-row>
+                <br>
+                <hr>
+                <br>
+                <el-row >
+                    <!-- <el-col :span="8">
+                    <div class="grid-content1 bg-purple1" style="margin-right:2vh">
+                        <el-image style="width: 100%; height:130px" :src="src2"></el-image>
+                    </div>
+                    </el-col> -->
+                    <el-col :span="24">
+                    <div class="grid-content1 bg-purple-light1">
+                        <!-- <b style="font-size:18px">开放带动，合作共赢</b> -->
+                        <p style="font-size:15px">广西需保障财税政策供给 助力数字经济建设</p>
+                        <el-button type="primary" size="small"><a href="http://www.gxzf.gov.cn/zt/jd/szgxzl/gdxw/20190530-750241.shtml" target="_blank">查看详情</a></el-button>
+                    </div>
+                    </el-col>
+                </el-row>
+                <br>
+                <hr>
+                <br>
+                <el-row >
+                    <!-- <el-col :span="8">
+                    <div class="grid-content1 bg-purple1" style="margin-right:2vh">
+                        <el-image style="width: 100%; height:130px" :src="src2"></el-image>
+                    </div>
+                    </el-col> -->
+                    <el-col :span="24">
+                    <div class="grid-content1 bg-purple-light1">
+                        <!-- <b style="font-size:18px">开放带动，合作共赢</b> -->
+                        <p style="font-size:15px">广西壮族自治区人民政府办公厅印发关于加快数字广西建设若干措施的通知</p>
+                        <el-button type="primary" size="small"><a href="http://www.gxzf.gov.cn/zwgk/zfwj/20180917-713413.shtml" target="_blank">查看详情</a></el-button>
+                    </div>
+                    </el-col>
+                </el-row>
+                <br>
+                <hr>
+                <br>
+                <el-row >
+                    <!-- <el-col :span="8">
+                    <div class="grid-content1 bg-purple1" style="margin-right:2vh">
+                        <el-image style="width: 100%; height:130px" :src="src2"></el-image>
+                    </div>
+                    </el-col> -->
+                    <el-col :span="24">
+                    <div class="grid-content1 bg-purple-light1">
+                        <!-- <b style="font-size:18px">开放带动，合作共赢</b> -->
+                        <p style="font-size:15px">广西壮族自治区大数据发展局关于印发2019年全区大数据发展和政务服务工作要点的通知</p>
+                        <el-button type="primary" size="small"><a href="http://www.gxzf.gov.cn/zt/jd/szgxzl/xgwj/20190412-743543.shtml" target="_blank">查看详情</a></el-button>
+                    </div>
+                    </el-col>
+                </el-row>
+                <br>
+                <hr>
+                <br>
+                <el-row >
+                    <!-- <el-col :span="8">
+                    <div class="grid-content1 bg-purple1" style="margin-right:2vh">
+                        <el-image style="width: 100%; height:130px" :src="src2"></el-image>
+                    </div>
+                    </el-col> -->
+                    <el-col :span="24">
+                    <div class="grid-content1 bg-purple-light1">
+                        <!-- <b style="font-size:18px">开放带动，合作共赢</b> -->
+                        <p style="font-size:15px">广西壮族自治区人民政府办公厅关于印发数字广西“广电云”村村通户户用工程三年攻坚会战实施方案（2018—2020年）的通知</p>
+                        <el-button type="primary" size="small"><a href="http://www.gxzf.gov.cn/zwgk/zfwj/20180917-713389.shtml" target="_blank">查看详情</a></el-button>
+                    </div>
+                    </el-col>
+                </el-row>
+                <br>
+                <hr>
+                <br> 
+                <el-row >
+                    <!-- <el-col :span="8">
+                    <div class="grid-content1 bg-purple1" style="margin-right:2vh">
+                        <el-image style="width: 100%; height:130px" :src="src2"></el-image>
+                    </div>
+                    </el-col> -->
+                    <el-col :span="24">
+                    <div class="grid-content1 bg-purple-light1">
+                        <!-- <b style="font-size:18px">开放带动，合作共赢</b> -->
+                        <p style="font-size:15px">广西壮族自治区人民政府办公厅关于印发广西数字社会建设三年行动计划（2018—2020年）的通知</p>
+                        <el-button type="primary" size="small"><a href="http://www.gxzf.gov.cn/zwgk/zfwj/20180917-713381.shtml" target="_blank">查看详情</a></el-button>
+                    </div>
+                    </el-col>
+                </el-row>
+                <br>
+                <hr>
+                <br>
+            </div>
+            </el-col>
+        </el-row>
     </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            src1:'http://image.gxnews.com.cn/uploadpic/2018/08/30/6c5275b59a09de08b4e58e5985641190.jpg',
+            src2:'http://image.gxnews.com.cn/uploadpic/2018/09/12/eaec1d96c7e6d273e46492f77b173ec0.jpg',
+            src3:'http://d.gxzf.gov.cn/image/uploadpic/2018/09/17/0388e5cb85c384a36eef8c7eca6c7455.jpg',
+            src4:'http://image.gxnews.com.cn/uploadpic/2017/12/28/19dd883a1f05c736bd326cb76a9eb6cc.jpg',
+            src5:'http://image.gxnews.com.cn/uploadpic/2017/12/28/5996e74cfe6ee957cfc1301d22bc87fe.jpg',
+            src6:'http://image.gxnews.com.cn/uploadpic/2017/12/28/50a38a558b784d21fdb86d2cd850f33f.jpg',
+        }
+    },
     
 }
 </script>
 
 <style>
+ .el-col1 {
+    border-radius: 4px;
+  }
+  .bg-purple-dark1 {
+    background: #99a9bf;
+  }
+  .bg-purple1 {
+    background: rgb(246, 249, 252);
+  }
+  .bg-purple-light1 {
+    background: rgb(246, 249, 252);
+  }
+  .grid-content1 {
+    border-radius: 4px;
+    min-height: 36px;
 
+  }
+  .row-bg1 {
+    padding: 10px 0;
+    background-color: #f9fafc;
+
+  }
+  .bg-purple-light2 {
+    background: rgb(110, 177, 245);
+  }
+  .grid-content2 {
+    border-radius: 4px;
+    min-height: 36px;
+    text-align: center;
+    margin-top: 10px;
+    line-height: 30px;
+
+  }
+  .icon-warp{
+      font-size: 20px;
+  }
 </style>
