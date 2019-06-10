98494a9c4d2b919d0930716e36e443286d97b118
Your Name <email@example.com>
2019-06-10 22:10:25 +0800

    ss

--- components/index5/index5_2.vue
+++ components/index5/index5_2.vue
@@ -1,15 +1,183 @@
 <template>
-    <div class="index5_2">
-        index5_2
+    <div class="WisdomYl">
+        <div style="margin-left: 10vh;margin-right: 10vh">
+            <br>
+            <i class="el-icon-edit" style="font-size: 20px"> <b>智慧玉林</b></i>
+            <hr>
+            <br>
+            <i class="el-icon-star-off" style="font-size: 16px"> <b>智慧侨乡</b></i>
+            <hr>
+            <el-row :gutter="20" style=" margin-top: 10px">
+                <el-col :span="6">
+                <div class="grid-content bg-purple"> 
+                    <el-link href="https://www.sogou.com/link?url=DSOYnZeCC_oFmTickJ_wjzMkVIumkSMDxv33361K0LSteaJZoPMQrCXw5Pqtik-2" target="_blank" title="授旗仪式"><el-image style="width: 100%; height:130px" :src="src1"></el-image></el-link>
+                    <el-link href="https://www.sogou.com/link?url=DSOYnZeCC_oFmTickJ_wjzMkVIumkSMDxv33361K0LSteaJZoPMQrCXw5Pqtik-2" target="_blank" title="授旗仪式">授旗仪式</el-link>
+                </div>
+                </el-col>
+                <el-col :span="6">
+                <div class="grid-content bg-purple">
+                    <el-link href="https://baike.sogou.com/v8443782.htm" target="_blank" title="都峤山"><el-image style="width: 100%; height:130px" :src="src2"></el-image></el-link>
+                    <el-link href="https://baike.sogou.com/v8443782.htm" target="_blank" title="都峤山">都峤山</el-link>
+                </div>
+                </el-col>
+                <el-col :span="6">
+                <div class="grid-content bg-purple">
+                    <el-link href="https://baike.sogou.com/v441012.htm" target="_blank" title="真武阁"><el-image style="width: 100%; height:130px" :src="src3"></el-image></el-link>
+                    <el-link href="https://baike.sogou.com/v441012.htm" target="_blank" title="真武阁">真武阁</el-link>
+                </div>
+                </el-col>
+                <el-col :span="6">
+                <div class="grid-content bg-purple">
+                    <el-link href="http://www.sohu.com/a/239514158_100019048" target="_blank" title="工作交流会"><el-image style="width: 100%; height:130px" :src="src4"></el-image></el-link>
+                    <el-link href="http://www.sohu.com/a/239514158_100019048" target="_blank" title="工作交流会">工作交流会</el-link>
+                </div>
+                </el-col>
+            </el-row>
+            
+            <br>
+            <i class="el-icon-star-off" style="font-size: 16px"> <b>农业大数据</b></i>
+            <hr>
+            <el-row :gutter="20" style=" margin-top: 10px">
+                <el-col :span="6">
+                <div class="grid-content bg-purple"> 
+                    <el-link href="http://www.tcloudit.com/Html/casedetail.html?ID=242" target="_blank" title="智慧农业"><el-image style="width: 100%; height:130px" :src="src5"></el-image></el-link>
+                    <el-link href="http://www.tcloudit.com/Html/casedetail.html?ID=242" target="_blank" title="智慧农业">智慧农业</el-link>
+                </div>
+                </el-col>
+                <el-col :span="6">
+                <div class="grid-content bg-purple">
+                    <el-link href="http://www.tcloudit.com/Html/casedetail.html?ID=242" target="_blank" title="智能环境监测"><el-image style="width: 100%; height:130px" :src="src6"></el-image></el-link>
+                    <el-link href="http://www.tcloudit.com/Html/casedetail.html?ID=242" target="_blank" title="智能环境监测">智能环境监测</el-link>
+                </div>
+                </el-col>
+                <el-col :span="6">
+                <div class="grid-content bg-purple">
+                    <el-link href="http://www.tcloudit.com/Html/casedetail.html?ID=242" target="_blank" title="视频监测"><el-image style="width: 100%; height:130px" :src="src7"></el-image></el-link>
+                    <el-link href="http://www.tcloudit.com/Html/casedetail.html?ID=242" target="_blank" title="视频监测">视频监测</el-link>
+                </div>
+                </el-col>
+                <el-col :span="6">
+                <div class="grid-content bg-purple">
+                    <el-link href="http://www.tcloudit.com/Html/casedetail.html?ID=242" target="_blank" title="农产品销售平台"><el-image style="width: 100%; height:130px" :src="src8"></el-image></el-link>
+                    <el-link href="http://www.tcloudit.com/Html/casedetail.html?ID=242" target="_blank" title="农产品销售平台">农产品销售平台</el-link>
+                </div>
+                </el-col>
+            </el-row>
+
+            <br>
+            <i class="el-icon-star-off" style="font-size: 16px"> <b>中药特色产业</b></i>
+            <hr>
+            <el-row :gutter="20" style=" margin-top: 10px">
+                <el-col :span="6">
+                <div class="grid-content bg-purple"> 
+                     <el-link href="http://news.gxnews.com.cn/staticpages/20180914/newgx5b9b9b41-17647456.shtml" target="_blank" title="中国（玉林）中医博览会"><el-image style="width: 100%; height:130px" :src="src9"></el-image></el-link>
+                    <el-link href="http://news.gxnews.com.cn/staticpages/20180914/newgx5b9b9b41-17647456.shtml" target="_blank" title="中国（玉林）中医博览会">中国（玉林）中医博览会</el-link>
+                </div>
+                </el-col>
+                <el-col :span="6">
+                <div class="grid-content bg-purple">
+                    <el-link href="http://www.gxnews.com.cn/staticpages/20170627/newgx5951d9af-16306725.shtml" target="_blank" title="玉林银丰国际中药港"><el-image style="width: 100%; height:130px" :src="src10"></el-image></el-link>
+                    <el-link href="http://www.gxnews.com.cn/staticpages/20170627/newgx5951d9af-16306725.shtml" target="_blank" title="玉林银丰国际中药港">玉林银丰国际中药港</el-link>
+                </div>
+                </el-col>
+                <el-col :span="6">
+                <div class="grid-content bg-purple">
+                    <el-link href="http://www.gxnews.com.cn/staticpages/20170627/newgx5951d9af-16306725.shtml" target="_blank" title="玉林中药材市场"><el-image style="width: 100%; height:130px" :src="src11"></el-image></el-link>
+                    <el-link href="http://www.gxnews.com.cn/staticpages/20170627/newgx5951d9af-16306725.shtml" target="_blank" title="玉林中药材市场">玉林中药材市场</el-link>
+                </div>
+                </el-col>
+                <el-col :span="6">
+                <div class="grid-content bg-purple">
+                    <el-link href="http://ku.m.chinanews.com/wapapp/zaker/cj/2018/09-14/8627859.shtml" target="_blank" title="药材产品展示"><el-image style="width: 100%; height:130px" :src="src12"></el-image></el-link>
+                    <el-link href="http://ku.m.chinanews.com/wapapp/zaker/cj/2018/09-14/8627859.shtml" target="_blank" title="药材产品展示">药材产品展示</el-link>
+                </div>
+                </el-col>
+            </el-row>
+
+            <br>
+            <i class="el-icon-star-off" style="font-size: 16px"> <b>玉林5G时代</b></i>
+            <hr>
+            <el-row :gutter="20" style=" margin-top: 10px">
+                <el-col :span="6">
+                <div class="grid-content bg-purple"> 
+                    <el-link href="http://www.gxylnews.com/html/news/2019/05/179511.html" target="_blank" title="玉林首个5G通话"><el-image style="width: 100%; height:130px" :src="src13"></el-image></el-link>
+                    <el-link href="http://dy.163.com/v2/article/detail/EEJ0ORGL05129O3K.html" target="_blank" title="电信5G体验馆">电信5G体验馆</el-link>
+                </div>
+                </el-col>
+                <el-col :span="6">
+                <div class="grid-content bg-purple">
+                    <el-link href="http://www.gxylnews.com/html/news/2019/05/179511.html" target="_blank" title="玉林首个5G通话"><el-image style="width: 100%; height:130px" :src="src14"></el-image></el-link>
+                    <el-link href="http://www.gxylnews.com/html/news/2019/05/179511.html" target="_blank" title="玉林首个5G通话">玉林首个5G通话</el-link>
+                </div>
+                </el-col>
+                <el-col :span="6">
+                <div class="grid-content bg-purple">
+                    <el-link href="http://www.sohu.com/a/315255128_179119" target="_blank" title="华为5G手机"><el-image style="width: 100%; height:130px" :src="src15"></el-image></el-link>
+                    <el-link href="http://www.sohu.com/a/315255128_179119" target="_blank" title="华为5G手机">华为5G手机</el-link>
+                </div>
+                </el-col>
+                <el-col :span="6">
+                <div class="grid-content bg-purple">
+                    <el-link href="http://gx.news.163.com/19/0508/10/EEL9OSVD04408DER.html" target="_blank" title="电信5G"><el-image style="width: 100%; height:130px" :src="src16"></el-image></el-link>
+                    <el-link href="http://gx.news.163.com/19/0508/10/EEL9OSVD04408DER.html" target="_blank" title="电信5G">电信5G</el-link>
+                </div>
+                </el-col>
+            </el-row>
+            <br>
+
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+        data(){
+        return{
+          src1:'http://5b0988e595225.cdn.sohucs.com/images/20180705/981c471786994142be4b8cbc36fd3c4e.jpeg',
+          src2:'https://pic.baike.soso.com/ugc/baikepic2/7081/20150323101848-1118155232.jpg/0',
+          src3:'https://pic.baike.soso.com/ugc/baikepic2/8998/20150119110206-672973658.jpg/0',
+          src4:'http://5b0988e595225.cdn.sohucs.com/images/20180705/da6aa5958ffc4c039113cef66012acf0.jpeg',
+          src5:'http://www.tcloudit.com/ueditor/net/upload/image/20171101/6364515089181128557164171.JPG',
+          src6:'http://www.tcloudit.com/ueditor/net/upload/image/20171101/6364515098664994208005680.JPG',
+          src7:'http://www.tcloudit.com/ueditor/net/upload/image/20171101/6364515107007233701124638.JPG',
+          src8:'http://www.tcloudit.com/ueditor/net/upload/image/20171101/6364515108863755507796500.png',
+          src9:'http://image.gxnews.com.cn/uploadpic/2018/09/14/5c0c8afdceda7bdedf528c464c48d797.jpg',
+          src10:'http://image.gxnews.com.cn/uploadpic/2017/06/27/380db86ed9fd48d50caedfdcbec5b6d4.jpg',
+          src11:'http://image.gxnews.com.cn/uploadpic/2017/06/27/00fed493bd2f8671211268e3dcb35067.jpg',
+          src12:'http://i2.chinanews.com/simg/2018/180914//88321240.jpg',
+          src13:'http://dingyue.ws.126.net/qh48pDZpo60z6Z9OH0OGKyeqRWFQ5V8NJPIxXVFO8J=k31557207062622.jpg',
+          src14:'http://www.gxylnews.com/uploadfile/2019/0520/1558329886419990.jpg',
+          src15:'http://5b0988e595225.cdn.sohucs.com/images/20190520/495ab0ea5ea448c0b796970660fdf2b4.jpeg',
+          src16:'http://dingyue.ws.126.net/YG6=fFXGvuurm8Lgbzn3KRojo8CFL8NOBLCVK=qGaMhqa1557283533870compressflag.jpg',
+        }
+    }
     
 }
 </script>
 
 <style>
-
+ .WisdomYl{
+    background-color: rgb(246, 249, 252);
+ }
+ .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: rgb(246, 249, 252);
+  }
+  .bg-purple-light {
+    background: rgb(246, 249, 252);
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+    text-align: center;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
 </style>
